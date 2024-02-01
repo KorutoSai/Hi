4d75e53869cca103c1606e78d9231550d73bb62e
KorutoSai <149676054+KorutoSai@users.noreply.github.com>
2024-02-01 22:48:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var name = prompt("What is your name?")
+
+var name1 = name.slice(0,1)
+
+var name2 = name1.toUpperCase()
+
+var name2a = name.length
+
+var name3 = name.slice(1,name2a)
+
+var name4 = name3.toLowerCase()
+
+alert("Hi " + name2 + name4 + "! penge pera.")
